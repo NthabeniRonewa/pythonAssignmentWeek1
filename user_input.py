7b5552a61470abd6a06f63821f65126c45a6c2e3
Nthabeni Ronewa <nthabenironewa@gmail.com>
2024-11-05 11:03:01 +0200

    Week 1 Assignment

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name" + "\n")
+age = input("Please enter your age" + "\n")
+location = input("Please enter your location" + "\n")
+
+
+print("Hello " + name +  ", you are " +  age + " years old " + " and you live in " + location)
